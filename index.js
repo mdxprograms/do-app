e8874fdf54cd2404b96b81bf4333ed3e43dbffe0
Josh Waller <mdx.programs@gmail.com>
2020-12-08 01:37:30 -0500

    organize

--- index.js
+++ index.js
@@ -1,69 +1,7 @@
-const figlet = require("figlet");
-const chalk = require("chalk");
 const clear = require("clear");
-const inquirer = require("inquirer");
 
-const colorStates = {
-  normal: (msg) => console.log(chalk.yellow(msg)),
-  error: (msg) => console.log(chalk.red(msg)),
-  info: (msg) => console.log(chalk.blue(msg)),
-  success: (msg) => console.log(chalk.green(msg)),
-};
-
-const appChoices = [
-  {
-    name: "react",
-    options: {
-      npm: "--use-npm",
-      typescript: "--template typescript",
-    },
-    command: (appName, options) => `npx create-react-app ${appName} ${options}`,
-  },
-  {
-    name: "vue",
-    options: {},
-    command: "",
-  },
-];
-
-const getOptions = async (appName) => {
-  const { options } = appChoices.find((a) => a.name === appName);
-  return Object.keys(options).map((o) => ({
-    name: o,
-    value: options[o],
-  }));
-};
-
-const appChoiceQ = async () =>
-  inquirer.prompt({
-    name: "appChoice",
-    type: "list",
-    choices: appChoices.map((c) => ({
-      name: c.name,
-      value: c,
-    })),
-  });
-
-const appOptionsQ = async (appName) =>
-  inquirer.prompt({
-    name: "appOptions",
-    type: "checkbox",
-    choices: await getOptions(appName),
-  });
-
-const appNameQ = async () =>
-  inquirer.prompt({
-    name: "appName",
-    type: "input",
-  });
-
-const introMsg = () =>
-  colorStates.info(
-    figlet.textSync("Do App", {
-      font: "Arrows",
-      horizontalLayout: "fitted",
-    })
-  );
+const { appChoiceQ, appOptionsQ, appNameQ } = require("./lib/questions");
+const { introMsg } = require("./lib/messages");
 
 const run = async () => {
   clear();
